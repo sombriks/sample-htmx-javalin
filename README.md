d31fdf37b46c8892e922b36ac1bf86bda3d02efb
Leonardo Silveira <sombriks@gmail.com>
2024-02-13 15:32:07 -0300

    initial import

---
+++ README.md
@@ -0,0 +1,19 @@
+# sample-htmx-javalin
+
+Sample application showing how htmx teams up with javalin
+
+## Dependencies
+
+- java 21
+- kotlin 1.9
+- htmx 2.x
+
+## How to build
+
+## How to run
+
+## Noteworthy
+
+## Next steps
+
+## Reference
---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+}
+
+group = "sample.htmx.javalin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.javalin:javalin:6.0.1")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
