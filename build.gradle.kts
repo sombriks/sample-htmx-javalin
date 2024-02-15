82fc15f3e6d393c65fc6918e34386c539c6125f7
Leonardo Silveira <sombriks@gmail.com>
2024-02-14 23:35:11 -0300

    proper htmx version

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
     implementation("io.javalin:javalin:6.0.1")
     implementation("io.javalin:javalin-rendering:6.0.1")
     implementation("org.apache.velocity:velocity-engine-core:2.3")
-    implementation("org.webjars.npm:htmx.org:1.9.10")
+    implementation("org.webjars.npm:htmx.org:2.0.0-alpha1")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
     implementation("org.jdbi:jdbi3-kotlin-sqlobject:3.44.1")
